e70547f761f6ed965f12b9c7a1a36c38d54c7a98
Valdemaras <valdemarasged@gmail.com>
2024-12-07 23:29:40 +0200

    add more calendar features

--- day6/calendar.js
+++ day6/calendar.js
@@ -1,10 +1,9 @@
-console.log("hello");
 const date = new Date();
 const year = date.getFullYear();
 const month = date.getMonth();
 const day = date.getDate();
 const daysInMonth = getDaysInMonth(year, month);
-const firstDayOfMonth = getFirstDayOfMonth(year, month);
+let firstDayOfMonth = getFirstDayOfMonth(year, month);
 const dayOfWeek = [
   "Sunday",
   "Monday",
@@ -35,7 +34,7 @@ function getDaysInMonth(year, month) {
 }
 
 function getFirstDayOfMonth(year, month) {
-  return new Date(year, month, 1).getDay();
+  return new Date(year, month).getDay();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -45,10 +44,41 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById(
     "current-month"
   ).textContent = `${monthNames[month]}, ${year}`;
-  //   for (let i = 1; i <= daysInMonth; i++) {
-  //     const dayElement = document.createElement("div");
-  //     dayElement.classList.add("calendar__day");
-  //     dayElement.textContent = `${i}`;
-  //     document.querySelector(".calendar__days").appendChild(dayElement);
-  //   }
+
+  document.querySelectorAll(".calendar__date").forEach((targetElement) => {
+    // week starts not from sunday here
+    if (firstDayOfMonth === 0) {
+      firstDayOfMonth = 7;
+    }
+    console.log(firstDayOfMonth);
+    for (let i = 1; i < firstDayOfMonth; i++) {
+      const placeholderDay = document.createElement("div");
+      placeholderDay.classList.add("calendar__number");
+      placeholderDay.textContent = "";
+      targetElement.appendChild(placeholderDay);
+    }
+
+    for (let i = 1; i <= daysInMonth; i++) {
+      const dayElement = document.createElement("div");
+      if (i === day) {
+        const dayElement = document.createElement("div");
+        dayElement.classList.add("calendar__number--current");
+        dayElement.textContent = `${i}`;
+        targetElement.appendChild(dayElement);
+        continue;
+      }
+      dayElement.classList.add("calendar__number");
+      dayElement.textContent = `${i}`;
+      targetElement.appendChild(dayElement);
+    }
+  });
 });
+
+const date2 = new Date();
+
+const year2 = date2.getFullYear();
+const month2 = date2.toLocaleDateString("default", { month: "long" });
+const weekday2 = date2.toLocaleDateString("default", { weekday: "long" });
+const day2 = date2.getDay() + 1;
+
+console.log(` my current date : ${firstDayOfMonth}`);
