f4116e1c27a11684510c7f8bb384caac725f4f47
Valdemaras <valdemarasged@gmail.com>
2024-12-22 23:21:27 +0200

    replaced repetitive query selectors to functions

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -4,9 +4,17 @@ function getRandomNumber() {
   return Math.floor(Math.random() * 20) + 1;
 }
 
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
 let secretNumber = getRandomNumber();
 let score = 20;
 let highScore = 0;
+
+const displayScore = function () {
+  document.querySelector(".score").textContent = "Score: " + score;
+};
 
 function newRandomNumber() {
   secretNumber = getRandomNumber();
@@ -15,39 +23,40 @@ function newRandomNumber() {
 
 console.log(secretNumber);
 
-document.querySelector(".score").textContent = "Score: " + score;
-
+displayScore();
 document.querySelector(".check-button").addEventListener("click", function () {
   const guess = Number(document.querySelector(".number-input").value);
   console.log(guess);
   if (!guess) {
-    document.querySelector(".message").textContent = "No Number!";
+    displayMessage("No Number!");
   } else if (guess === secretNumber) {
     highScore = document.querySelector(".highscore").textContent.split(": ")[1];
     console.log(highScore);
     if (score > highScore) {
       document.querySelector(".highscore").textContent = `Highscore: ${score}`;
     }
-    document.querySelector(".message").textContent = "You Win!!";
+    document.querySelector(".square").textContent = secretNumber;
+    displayMessage("You Win!!");
     document.querySelector("body").style.backgroundColor = "#60b347";
   } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "Too Low";
+    displayMessage("Too Low");
     score--;
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "Too High";
+    displayMessage("Too High");
     score--;
   }
   if (score === 0) {
-    document.querySelector(".message").textContent = "You lost the game!";
+    displayMessage("You lost the game!");
   }
-  document.querySelector(".score").textContent = "Score: " + score;
+  displayScore();
 });
 
 document.querySelector(".again-button").addEventListener("click", function () {
   newRandomNumber();
   document.querySelector("body").style.backgroundColor = "#ffffff";
   document.querySelector(".number-input").value = "";
-  document.querySelector(".message").textContent = "Start guessing...";
+  displayMessage("Start guessing...");
   score = 20;
-  document.querySelector(".score").textContent = "Score: " + score;
+  displayScore();
+  document.querySelector(".square").textContent = "?";
 });
